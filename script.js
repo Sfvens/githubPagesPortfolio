b8f8ba5cc8a5d1cebb2c29c9cc7be90e6f4f29fa
Sfvens <carlsensam1@gmail.com>
2024-11-14 17:11:36 -0700

    find out where the root is in github pages

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ var currentScrollPos = window.scrollY;
   }
   prevScrollpos = currentScrollPos;
 }
+var loc = window.location.pathname;
+var dir = loc.substring(0, loc.lastIndexOf('/'));
+console.log(loc)
+console.log(dir)
