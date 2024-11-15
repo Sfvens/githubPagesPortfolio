98317066be191bfdc9e20ec27ead889e1f7ee1a5
Sfvens <carlsensam1@gmail.com>
2024-11-14 17:05:47 -0700

    basic outline complete

---
+++ script.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-80px"; /* adjust this value to the height of your header */
+  }
+  prevScrollpos = currentScrollPos;
+}
