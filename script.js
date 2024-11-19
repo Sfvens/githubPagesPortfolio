f38d2b8dd2863f971812c668821e522bd1e9ace4
Sfvens <carlsensam1@gmail.com>
2024-11-18 18:27:08 -0700

    complete splash

--- script.js
+++ script.js
@@ -4,14 +4,9 @@ window.onscroll = function() {
   let currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
-    } else if(currentScrollPos > 400) { // make sure this value is set the the height of your intro
+    } else { // make sure this value is set the the height of your intro
     document.getElementById("header").style.top = "-50px"; /* adjust this value to the height of your header */
   }
   prevScrollpos = currentScrollPos;
-
-  //logic for parallax effect
-  // if start or final position > size of window, do nothing
-  // else override regular scroll behavior and change the size of the
-  // window by final-start (negative if going down, positive if going up)
 }
 
